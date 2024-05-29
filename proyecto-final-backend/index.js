00e80a1d52d10173738c95f6d2169346d23b6bd5
JOSE_ANGEL JIMENEZ_CONDE <jajimenez@mulhacensoft.com>
2024-05-29 11:29:50 +0200

    Actualizar configuración de Vercel para el frontend

--- proyecto-final-backend/index.js
+++ proyecto-final-backend/index.js
@@ -19,16 +19,11 @@ app.use('/api/juegos', juegosRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/comentarios', comentariosRoutes);
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('No funciona!');
-});
+// Exportar la aplicacion
+module.exports = app;
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-
-// Exportar la aplicacion
-module.exports = app;
